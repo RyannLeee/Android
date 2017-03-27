0dff8eb33630f3b8848ab4f00e2ae2d825001978
YuanLee0214 <YuanLee0214@outlook.com>
2017-03-27 20:58:59 +0800

    Android Project

--- BroadCast/app/src/main/java/com/example/broadcast/MainActivity.java
+++ BroadCast/app/src/main/java/com/example/broadcast/MainActivity.java
@@ -8,6 +8,8 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,6 +26,15 @@ public class MainActivity extends AppCompatActivity {
         intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
         networkChangeReceiver = new NetworkChangeReceiver();
         registerReceiver(networkChangeReceiver, intentFilter);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new
+                        Intent("com.example.broadcast.MY_BROADCAST");
+                sendBroadcast(intent);
+            }
+        });
     }
 
     @Override
