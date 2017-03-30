344afefedbc28ab131a9f1617dfa5e663b9c67d0
yuanlee0214 <yuanlee0214@outlook.com>
2017-03-30 22:09:41 +0800

    Android Project

---
+++ LitePalTest/app/src/main/java/com/example/litepaltest/Category.java
@@ -0,0 +1,26 @@
+package com.example.litepaltest;
+
+/**
+ * Created by Y-Lee on 2017/3/30.
+ */
+
+public class Category {
+
+    private int id;
+
+    private String categoryName;
+
+    private int categoryCode;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public void setCategoryCode(int categoryCode) {
+        this.categoryCode = categoryCode;
+    }
+}
