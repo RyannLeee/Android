2af2d2180d368aba6d7cd67724209eb3647a1427
yuanlee0214 <yuanlee0214@outlook.com>
2017-04-04 11:55:52 +0800

    Android Project

--- 9th/WebViewTest/app/src/main/java/com/example/webviewtest/MainActivity.java
+++ 9th/WebViewTest/app/src/main/java/com/example/webviewtest/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.webviewtest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
