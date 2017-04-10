25df7f5088f4ec0a933c638bdd5f5482e9154d81
yuanlee0214 <yuanlee0214@outlook.com>
2017-04-10 19:20:24 +0800

    Android Project

---
+++ 12th/MaterialTest/app/src/main/java/com/example/materialtest/Fruit.java
@@ -0,0 +1,26 @@
+package com.example.materialtest;
+
+/**
+ * Created by Y-Lee on 2017/4/10.
+ */
+
+public class Fruit {
+
+    private String name;
+
+    private int imageId;
+
+    public Fruit(String name, int imageId) {
+        this.name = name;
+        this.imageId = imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+}
