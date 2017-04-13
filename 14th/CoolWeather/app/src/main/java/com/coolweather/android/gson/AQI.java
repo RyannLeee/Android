40a9670d50a7b4aeba18b99016b62364ff2fce55
yuanlee0214 <yuanlee0214@outlook.com>
2017-04-13 20:59:42 +0800

    Android Project

---
+++ 14th/CoolWeather/app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,19 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by Y-Lee on 2017/4/13.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+
+}
