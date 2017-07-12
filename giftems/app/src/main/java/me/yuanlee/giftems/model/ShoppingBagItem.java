79f3e94be8c7e154dee9953650f02b8ac0be223e
yuanlee0214 <yuanlee0214@outlook.com>
2017-07-12 22:14:47 +0800

    Android Project

---
+++ giftems/app/src/main/java/me/yuanlee/giftems/model/ShoppingBagItem.java
@@ -0,0 +1,11 @@
+package me.yuanlee.giftems.model;
+
+/**
+ * Created by Y-Lee on 2017.07.12.
+ */
+
+public class ShoppingBagItem {
+    public int giftStyleId;
+    public int quantity;
+    public GiftStyle giftStyle;
+}
