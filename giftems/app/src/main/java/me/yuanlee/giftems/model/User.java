82aa3650aad84d668829f65fe8178240d792f611
yuanlee0214 <yuanlee0214@outlook.com>
2017-07-12 21:03:37 +0800

    Android Project

---
+++ giftems/app/src/main/java/me/yuanlee/giftems/model/User.java
@@ -0,0 +1,17 @@
+package me.yuanlee.giftems.model;
+
+/**
+ * Created by Y-Lee on 2017.07.12.
+ */
+
+public class User {
+    public int id;
+    public String name;
+    public String mobile;
+    public String mobileToken;
+    public String realName;
+    public String sex;
+    public String birthday;
+    public String work;
+    public Integer bindStatus;
+}
