76e6c950627447021e86067efe6a277ee282235d
yuanlee0214 <yuanlee0214@outlook.com>
2017-07-16 17:15:46 +0800

    Android Project

---
+++ giftems/app/src/main/java/me/yuanlee/giftems/App.java
@@ -0,0 +1,13 @@
+package me.yuanlee.giftems;
+
+import android.app.Application;
+
+import me.yuanlee.giftems.model.User;
+
+/**
+ * Created by Y-Lee on 2017.07.16.
+ */
+
+public class App extends Application {
+    public User user;
+}
