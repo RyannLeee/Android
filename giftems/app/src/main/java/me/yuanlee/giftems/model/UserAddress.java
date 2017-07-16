a9f6511fcfec30062fefa58b06cfce1901371212
yuanlee0214 <yuanlee0214@outlook.com>
2017-07-16 16:27:37 +0800

    Android Project

---
+++ giftems/app/src/main/java/me/yuanlee/giftems/model/UserAddress.java
@@ -0,0 +1,21 @@
+package me.yuanlee.giftems.model;
+
+/**
+ * Created by Y-Lee on 2017.07.16.
+ */
+
+public class UserAddress {
+    public int id;
+    public int userId;
+    public String name;
+    public String mobile;
+    public int provinceId;
+    public int cityId;
+    public int areaId;
+    public String address;
+    public String postcode;
+    public String info;
+    public String provinceName;
+    public String cityName;
+    public String areaName;
+}
